1207f8b29ef8c2bffe1731a6093aa9a7444c5e10
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-14 12:43:34 -0300

    Implementando as classes restantes

---
+++ ProjetoXadrez/tabuleiro/Cor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoXadrez.tabuleiro {
+    enum Cor {
+        Branca,
+        Preta,
+        Amarela, 
+        Azul,
+        Vermelha
+    }
+}
---
+++ ProjetoXadrez/tabuleiro/Peca.cs
@@ -0,0 +1,16 @@
+﻿using ProjetoXadrez.tabuleiro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro {
+    class Peca {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; set; }
+        public Tabuleiro tabuleiro { get; set; }
+    }
+}
+
