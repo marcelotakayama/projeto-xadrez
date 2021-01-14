b0695b1cd83127863a04038ae4e67281e520f7ae
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-14 16:44:03 -0300

    Imprimindo as peças pretas e brancas funcionando

--- ProjetoXadrez/Program.cs
+++ ProjetoXadrez/Program.cs
@@ -9,23 +9,21 @@ using System.Threading.Tasks;
 namespace ProjetoXadrez {
     class Program {
         static void Main(string[] args) {
-            /*
+           
             try {
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(3, 5));
 
                 Tela.imprimirTabuleiro(tab);
             } catch (TabuleiroException e) {
                 Console.WriteLine(e.Message);
             }
-            */
-
-            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosicao());
+            
 
             Console.ReadLine();
         }
