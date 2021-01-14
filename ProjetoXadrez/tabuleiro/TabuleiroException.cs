7d2a30295fa969c8dde9c28762d0ce61e3ed4434
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-14 15:27:53 -0300

    Bug resolvido

--- ProjetoXadrez/tabuleiro/TabuleiroException.cs
+++ ProjetoXadrez/tabuleiro/TabuleiroException.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace tabuleiro {
     class TabuleiroException : Exception {
-        public TabuleiroException(string msg) : base(msg);
+        public TabuleiroException(string msg) : base(msg) {
+        }
     }
 }
