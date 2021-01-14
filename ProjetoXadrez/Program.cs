2c883acc04130f59a50872fe05f07f735b93f548
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-14 13:23:28 -0300

    Imprimindo o tabuleiro funciona

--- ProjetoXadrez/Program.cs
+++ ProjetoXadrez/Program.cs
@@ -10,6 +10,7 @@ namespace ProjetoXadrez {
         static void Main(string[] args) {
             Tabuleiro tab = new Tabuleiro(8, 8);
 
+            Tela.imprimirTabuleiro(tab);
             Console.ReadLine();
         }
     }
