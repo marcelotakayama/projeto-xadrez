0bdac589ec901b9c9382e86cbb6eb6e367ead92b
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-14 14:40:42 -0300

    Subindo com algumas coisas faltando mas ja vou arrumar

--- ProjetoXadrez/Program.cs
+++ ProjetoXadrez/Program.cs
@@ -10,6 +10,10 @@ namespace ProjetoXadrez {
         static void Main(string[] args) {
             Tabuleiro tab = new Tabuleiro(8, 8);
 
+            tab.colocarPeca(?, new Posicao(0, 0));
+            tab.colocarPeca(?, new Posicao(1, 3));
+            tab.colocarPeca(?, new Posicao(2, 4));
+
             Tela.imprimirTabuleiro(tab);
             Console.ReadLine();
         }
