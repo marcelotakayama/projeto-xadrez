c6d3626385457bdfa7fc23daf1245d07eaeb95c9
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-14 16:59:21 -0300

    Implementando classe partidadexadrez

--- ProjetoXadrez/xadrez/PartidaDeXadrez.cs
+++ ProjetoXadrez/xadrez/PartidaDeXadrez.cs
@@ -3,8 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tabuleiro;
 
 namespace xadrez {
     class PartidaDeXadrez {
+
+        private Tabuleiro tab;
+        private int turno;
+        private Cor jogadorAtual;
+
+        public PartidaDeXadrez() {
+            tab = new Tabuleiro(8, 8);
+            turno = 1;
+            jogadorAtual = Cor.Branca;
+        }
+
+        public void executaMovimento(Posicao origem, Posicao destino) {
+            Peca p = tab.
+        }
     }
 }
