e4396e04b7dad74471ad1883b73b37f304a15688
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-15 17:36:38 -0300

    Testando a função de xeque, funcionando normalmente

--- ProjetoXadrez/Tela.cs
+++ ProjetoXadrez/Tela.cs
@@ -16,6 +16,9 @@ namespace ProjetoXadrez {
             Console.WriteLine();
             Console.WriteLine("Turno: " + partida.turno);
             Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+            if (partida.xeque) {
+                Console.WriteLine("XEQUE!");
+            }
         }
 
         public static void imprimirPecasCapturadas(PartidaDeXadrez partida) {
