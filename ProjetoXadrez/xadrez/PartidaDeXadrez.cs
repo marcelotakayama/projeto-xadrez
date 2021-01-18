ec175c3452fd1177e8da3a33651b9aacce0c1876
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-18 11:26:57 -0300

    Iniciando a implementação da função xeque mate

--- ProjetoXadrez/xadrez/PartidaDeXadrez.cs
+++ ProjetoXadrez/xadrez/PartidaDeXadrez.cs
@@ -146,6 +146,22 @@ namespace xadrez {
             return false;
         }
 
+        public bool testeXequeMate(Cor cor) {
+            if (!estaEmXeque(cor){
+                return false;
+            }
+            foreach (Peca x in pecasEmJogo(cor)) {
+                bool[,] mat = x.movimentosPossiveis();
+                for(int i=0; i<tab.linhas; i++) {
+                    for(int j=0; j<tab.colunas; j++) {
+                        if(mat[i, j]) {
+                            Peca pecaCapturada = executaMovimento(x.posicao, new Posicao(i, j));
+                        }
+                    }
+                }
+            }
+        }
+
         public void colocarNovaPeca(char coluna, int linha, Peca peca) {
             tab.colocarPeca(peca, new PosicaoXadrez(coluna, linha).toPosicao());
             pecas.Add(peca);
